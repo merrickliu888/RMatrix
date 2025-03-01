81ed9adeed270438b34be00d8bcb359fd9cce1c8
Merrick Liu <merrickliu168@gmail.com>
2025-02-28 23:55:33 -0500

    basic matrix except mult

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
-pub trait Matrix {}
+pub trait Matrix {
+    fn zeroes(rows: usize, cols: usize) -> Self;
+
+    fn identity(size: usize) -> Self;
+
+    fn matrix_addition(&self, other: &Self) -> Self;
+
+    fn matrix_subtraction(&self, other: &Self) -> Self;
+
+    fn matrix_multiplication(&self, other: &Self) -> Self;
+
+    fn scalar_multiplication(&self, scalar: &f64) -> Self;
+
+    fn transpose(&mut self) -> ();
+}
 
 pub mod matrices;
--- src/matrices/basic_matrix.rs
+++ src/matrices/basic_matrix.rs
@@ -1,3 +1,105 @@
 use crate::Matrix;
 
-pub struct BasicMatrix {}
+pub struct BasicMatrix {
+    rows: usize,
+    cols: usize,
+    data: Vec<Vec<f64>>,
+    transposed: bool,
+}
+
+impl BasicMatrix {
+    pub fn new(rows: usize, cols: usize, data: Vec<Vec<f64>>) -> Self {
+        Self {
+            rows,
+            cols,
+            data,
+            transposed: false,
+        }
+    }
+}
+
+impl Matrix for BasicMatrix {
+    fn zeroes(rows: usize, cols: usize) -> Self {
+        Self {
+            rows,
+            cols,
+            data: vec![vec![0.0; cols]; rows],
+            transposed: false,
+        }
+    }
+
+    fn identity(size: usize) -> Self {
+        let mut data = vec![vec![0.0; size]; size];
+        for i in 0..size {
+            data[i][i] = 1.0;
+        }
+        Self {
+            rows: size,
+            cols: size,
+            data,
+            transposed: false,
+        }
+    }
+
+    fn matrix_addition(&self, other: &Self) -> Self {
+        if self.rows != other.rows || self.cols != other.cols {
+            panic!(
+                "Dimension mismatch - LHS ({}x{}), RHS ({}x{})",
+                self.rows, self.cols, other.rows, other.cols
+            );
+        }
+
+        let mut res = Self::zeroes(self.rows, self.cols);
+
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                res.data[i][j] = self.data[i][j] + other.data[i][j]
+            }
+        }
+
+        res
+    }
+
+    fn matrix_subtraction(&self, other: &Self) -> Self {
+        if self.rows != other.rows || self.cols != other.cols {
+            panic!(
+                "Dimension mismatch - LHS ({}x{}), RHS ({}x{})",
+                self.rows, self.cols, other.rows, other.cols
+            );
+        }
+
+        let mut res = Self::zeroes(self.rows, self.cols);
+
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                res.data[i][j] = self.data[i][j] - other.data[i][j]
+            }
+        }
+
+        res
+    }
+
+    fn matrix_multiplication(&self, other: &Self) -> Self {
+        let mut res = Self::zeroes(self.rows, self.cols);
+
+        // TODO
+
+        res
+    }
+
+    fn scalar_multiplication(&self, scalar: &f64) -> Self {
+        let mut res = Self::zeroes(self.rows, self.cols);
+
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                res.data[i][j] = self.data[i][j] * scalar;
+            }
+        }
+
+        res
+    }
+
+    fn transpose(&mut self) -> () {
+        self.transposed = !self.transposed;
+    }
+}
