4ea217dd6b829bab9cbe9f745a45f4acce8bb86b
Merrick Liu <merrickliu168@gmail.com>
2025-03-05 01:51:20 -0500

    simple benchmarks

---
+++ benches/criterion_benchmarks/basic_matrix.rs
@@ -0,0 +1,6 @@
+// use criterion::{black_box, criterion_group, criterion_main, Criterion};
+// use rmatrix::matrices::basic_matrix::BasicMatrix;
+// use rmatrix::Matrix;
+
+// criterion_group!(basic_matrix);
+// criterion_main!(basic_matrix);
--- benches/simple_benchmark.rs
+++ benches/simple_benchmark.rs
@@ -9,27 +9,41 @@ use std::fs::File;
 use std::io::BufReader;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let run_all = args.len() == 1;
+    println!("Starting benchmark...");
 
+    let args: Vec<String> = env::args().collect();
+    let run_all = args.contains(&String::from("all"));
+
+    println!("Loading matrices...");
     let matrices1 = load_matrices("./benches/matrices1.json");
     let matrices2 = load_matrices("./benches/matrices2.json");
+    println!("Matrices loaded.");
 
-    if run_all || args[1] == "basic_matrix" {
+    if !args.contains(&String::from("exclude_basic_matrix"))
+        && (run_all || args.contains(&String::from("basic_matrix")))
+    {
+        println!("Benchmarking basic matrix...");
         let basic_matrix_results = benchmark_basic_matrix(&matrices1, &matrices2);
         save_benchmark_results(
             "./benches/benchmark_results/basic_matrix_results.json",
             &basic_matrix_results,
         );
+        println!("basic matrix benchmark completed.");
     }
 
-    if run_all || args[1] == "ndarray_matrix" {
+    if !args.contains(&String::from("exclude_ndarray_matrix"))
+        && (run_all || args.contains(&String::from("ndarray_matrix")))
+    {
+        println!("Benchmarking ndarray matrix...");
         let ndarray_matrix_results = benchmark_ndarray_matrix(&matrices1, &matrices2);
         save_benchmark_results(
             "./benches/benchmark_results/ndarray_matrix_results.json",
             &ndarray_matrix_results,
         );
+        println!("ndarray matrix benchmark completed.");
     }
+
+    println!("Benchmark completed.");
 }
 
 fn load_matrices(filename: &str) -> Vec<Vec<Vec<f64>>> {
